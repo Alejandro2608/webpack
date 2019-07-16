6bc3710eed59e05ae85880d1410a7c157596c322
Sala 310 <sala310@poliacademico.edu.loc>
2019-07-16 10:42:37 -0500

    Actualización

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const autoprefixer = require('autoprefixer');
 module.exports = {
    entry: './src/app.js',
    output: {
-       path: path.resolve(__dirname, '../dist'),
+       path: path.resolve(__dirname, './dist'),
        filename: 'js/bundle.js'
    },
    devtool: 'source-map',
